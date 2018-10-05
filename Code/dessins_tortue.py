f722fbfa870cface62a8f9370aa476ad96f4756d
Kaman <Kaman16384@users.noreply.github.com>
2018-10-05 17:36:48 +0200

    Quelques fonctions créées et retouchées
    equilateral
    etoile5
    etoile6

--- Code/dessins_tortue.py
+++ Code/dessins_tortue.py
@@ -5,25 +5,42 @@ Created on Mon Oct  1 20:16:49 2018
 @author: Cyril GENISSON
 """
 from turtle import *
+from math import pi, sqrt
 
 # Exercice 7.*
 # Création de différentes fonctions de dessins
-couleur = ['blue', 'brown', 'black', 'red', 'yellow', 'green', 'purple']
+skin = ['blue', 'brown', 'black', 'red', 'yellow', 'green', 'purple']
 
-def equilateral(c, s):
+def equilateral(s, c, a=1):
     ''' Méthode pour dessiner un triangle équilatéral:
-        c: couleur
         s: taille en pixels
+        c: couleur
+        a: angle direct 1 ou indirect 0
     '''
     color(c)
     down()
     for k in range(3):
         forward(s)
-        left(120)
+        if a:
+            left(120)
+        else:
+            right(120)
     up()
-    forward(s+20)
+    forward(s)
 
-def carre(c, s):
+def etoile6(s, c):
+# Voir pour améliorer la fonction avec le moins d'instructions possibles
+    equilateral(s, c)
+    left(90)
+    forward(s / sqrt(3))
+    left(90)
+    equilateral(s, c)
+    left(90)
+    forward(s / sqrt(3))
+    left(90)
+    forward(s)
+
+def carre(s, c):
     ''' Méthode pour dessiner un carré:
         c: couleur
         s: taille en pixels
@@ -34,8 +51,13 @@ def carre(c, s):
         forward(s)
         left(90)
     up()
-    forward(s+20)
 
-reset()
-carre(couleur[3], 60)
-equilateral(couleur[0],50)
+def etoile5(s, c):
+    color(c)
+    down()
+    for k in range(5):
+        forward(s)
+        right(144)
+    forward(s)
+
+	
